14b0d6323850dd06268c7fb748a1610ffcee689e
BIMA <bima46bayu25@gmail..com>
2026-04-28 11:31:31 +0700

    fix: scheduler qeueu command

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -28,7 +28,8 @@ return Application::configure(basePath: dirname(__DIR__))
         // [QUEUE WORKER] Eksekusi antrean setiap menit lalu langsung berhenti jika kosong
         $schedule->command('queue:work --stop-when-empty')
                  ->everyMinute()
-                 ->withoutOverlapping();
+                 ->withoutOverlapping()
+                 ->runInBackground();
     })
     ->withMiddleware(function (Middleware $middleware): void {
 
